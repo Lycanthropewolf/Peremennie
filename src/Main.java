bfbf2375fe8bf9a2cada5a7b49989073ad2f7c57
Антон <antonolegovich2017@icloud.com>
2022-07-29 15:09:06 +0600

    переменные домашечка

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // задание 1
-        var box = 5;
-        System.out.println("box");
+        byte cat = 10;
+        short dog = 30_000;
+        int bird = -145_125_352;
+        long spider = 148_353_455_456_875L;
+        float people = 2.5F;
+        double money = 123.567;
+
+
+
+
     }
 }
