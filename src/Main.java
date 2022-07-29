7893a0eb77a1c4581dec4afccdef92194d5aa387
Антон <antonolegovich2017@icloud.com>
2022-07-29 15:29:22 +0600

    переменные домашечка

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
         // задание 1
         byte cat = 10;
         short dog = 30_000;
-        int bird = -145_125_352;
-        long spider = 148_353_455_456_875L;
+        int birdEat = 145_125_352;
+        long spiderWeight = 148_353_455_456_875L;
         float people = 2.5F;
         double money = 123.567;
+        boolean namberOfCats = cat >= 8;
+        System.out.println(namberOfCats);
 
 
 
