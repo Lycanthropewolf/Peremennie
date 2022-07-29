4f26d94d9555d54e4bfe5d5825a96bf95c346de8
Антон <antonolegovich2017@icloud.com>
2022-07-29 17:32:30 +0600

    переменные домашечка

--- src/Main.java
+++ src/Main.java
@@ -17,9 +17,9 @@ public class Main {
         float boxerA = 78.2F;
         float boxerB = 82.7F;
         float totalWeightOfBoxer = boxerA + boxerB;
-        System.out.println("Общий вес боксеров " + totalWeightOfBoxer);
+        System.out.println("Общий вес боксеров " + totalWeightOfBoxer + " килограмм");
         float differentWeightOfBoxer = boxerB - boxerA;
-        System.out.println("Разница в весе боксеров " + differentWeightOfBoxer);
+        System.out.println("Разница в весе боксеров " + differentWeightOfBoxer + " килограмм");
         // Задание 3
         byte banana = 5;
         short milk = 2;
@@ -30,19 +30,30 @@ public class Main {
         byte weightIceCream = 100;
         byte weightEggs = 70;
         double totalWeight = banana * weightBanana + milk * milkWeight100Ml + iceCream * weightIceCream + eggs * weightEggs;
-        System.out.println("Общий вес смеси в граммах " + totalWeight);
+        System.out.println("Общий вес смеси " + totalWeight + " грамм");
         double totalWeightKG = totalWeight / 1000;
-        System.out.println("Общий вес смеси в килограммах = " +totalWeightKG);
+        System.out.println("Общий вес смеси = " +totalWeightKG + " килограмм");
         // Задание 4
         int targetedWeightLoss = 7_000;
         int minimalWeightLossPerDay = 250;
         int maximumWeightLossPerDay = 500;
         int amountOfDayMin = targetedWeightLoss / maximumWeightLossPerDay;
         int amountOfDayMax = targetedWeightLoss / minimalWeightLossPerDay;
-        System.out.println("Минимальный срок похудения =" + amountOfDayMin);
-        System.out.println("Максимальный срок похудения =" + amountOfDayMax );
+        System.out.println("Минимальный срок похудения = " + amountOfDayMin + " дней");
+        System.out.println("Максимальный срок похудения = " + amountOfDayMax + " дней" );
         int averageWeightLoss = (amountOfDayMax + amountOfDayMin) / 2;
-        System.out.println("Средний срок похудения =" + averageWeightLoss );
+        System.out.println("Средний срок похудения =" + averageWeightLoss + " день" );
+        // Задание 5
+        int masha = 67_760;
+        int denis = 83_690;
+        int kristina = 76_230;
+        int summaZPDoPovisheniya = masha + denis + kristina;
+        double zpMashaPlus = ( masha * 0.1 ) + masha;
+        double zpDenisPlus = (denis * 0.1) + denis;
+        double zpKristinaPlus = (kristina * 0.1 ) + kristina;
+        double  summaZPPoslePovisheniya = zpMashaPlus + zpDenisPlus + zpKristinaPlus;
+        double raznica = summaZPPoslePovisheniya - summaZPDoPovisheniya;
+        System.out.println("разница между годовым доходом до и после повышения " + raznica + " рублей ");
 
 
 
